79598ecd2fb50ca3a15fdc9c7524cf7fe711d756
jasaapplab <137791896+jasaapplab@users.noreply.github.com>
2023-07-11 02:04:15 +0800

    menu food

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -24,31 +24,6 @@
 
             </ul>
 
-            <div class="dropdown-cart background-button me-2">
-                <a href="#" class="dropdown-toggle-item" id="cartDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <div class="icon-header-item icon-header-noti" data-notify="2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                            class="bi bi-bag-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
-                        </svg>
-                    </div>
-                </a>
-                <div class="dropdown-menu-item" aria-labelledby="cartDropdown">
-                  <ul class="cart-items">
-                    <!-- Daftar item dalam cart -->
-                    <li>
-                     {{-- item --}}
-                    </li>
-                  </ul>
-                  <div class="cart-footer">
-                    <p class="total-price">Total: $30.00</p>
-                    <a href="#" class="checkout-button">Checkout</a>
-                  </div>
-                </div>
-              </div>
-
-
             @guest
                 <div class="ps-md-3">
                     <a href="{{ route('auth') }}" class="btn btn-primary px-3 nav-item">MASUK</a>
